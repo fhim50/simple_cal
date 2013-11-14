79de3f4ead1f398b187bb531a7b772f788600b02
Kwaw <kwawannor@gmail.com>
2013-11-14 13:47:59 +0100

    Code cleanup

--- _calculator.py
+++ _calculator.py
@@ -15,16 +15,102 @@ class Interface:
 		return self.console
 
 
-class calculator:
+class Calculator:
 
 	def __init__(self):
-		pass
+		self.results = 0
+		self.pressed_key = ''
+		self.operator = ''
+		#flags
+		self.equal_press = False
+		self.pending_operation = False
+		self.key_press = True
 
-	def key_press(self):
-		pass
+	def change_state(self, flag):
+		return not flag
 
 
-def button(text, row, column, command = None, operator = None ):
+
+	def press_key(self, key):
+		self.equal_press = self.change_state(self.equal_press)
+		content = console.get()
+		key = str(key)
+		if self.key_press:
+			self.pressed_key = key
+			self.key_press = self.change_state(self.key_press)
+
+		else:
+			if key == '.' and key in content:
+				return
+			self.pressed_key = content + key
+
+		self.log(self.pressed_key)
+
+	def compute(self):
+		self.equal_press = self.change_state(self.equal_press)
+		self.pressed_key = float(self.pressed_key)
+		if self.pending_operation:
+			self.estimate()
+		else:
+			self.results = float(console.get())
+		# self.results = float(console.get())
+		# return self.estimate() if self.pending_operation else self.results
+
+
+	def log(self, value):
+		console.delete(0, END)
+		console.insert(0, value)
+
+	def estimate(self):
+		
+		value = self.pressed_key
+
+		if self.operator is '/':
+			self.results /= value
+
+		if self.operator is '-':
+			self.results -= value
+
+		if self.operator is '+':
+			self.results += value
+
+		if self.operator is '*':
+			self.results *= value
+
+		
+		self.key_press = self.change_state(self.key_press)
+		self.pending_operation = self.change_state(self.pending_operation)
+		self.log(self.results)
+
+	def operation(self, operator):
+		self.pressed_key = float(self.pressed_key)
+		if self.pending_operation: 
+			self.compute()
+
+		elif self.change_state(self.equal_press):
+			self.results = self.pressed_key
+		self.key_press = self.change_state(self.key_press)
+		self.pending_operation = self.change_state(self.pending_operation)
+		self.operator = operator
+		self.equal_press = self.change_state(self.equal_press)
+		
+			# self.pending_operation = True
+			# if self.equal_press:
+			# 	self.results = float(console.get())
+
+			# else:
+			# 	self.results = self.pressed_key
+			# 	self.operator = operator
+			# 	self.equal_press = False
+
+	def clear(self):
+		self.equal_press = self.change_state(self.equal_press)
+		self.pressed_key = '0'
+		self.log(0)
+		self.key_press = self.change_state(self.key_press)
+
+
+def button(text, row, column, command = None):
 	button = Button(window, text = text)
 	button['command'] = command
 	button.grid(row = row, column = column, pady = 3)
@@ -33,6 +119,8 @@ def button(text, row, column, command = None, operator = None ):
 
 
 interface = Interface()
+calculator = Calculator()
+
 window = interface.window
 
 #get display console
@@ -42,29 +130,29 @@ console = interface.input()
 #note a better version of this is to write a function
 #to generate all this buttons
 
-button('1', 1, 0)
-button('2', 1, 1)
-button('3', 1, 2)
+button('1', 1, 0, lambda: calculator.press_key(1))
+button('2', 1, 1, lambda: calculator.press_key(2))
+button('3', 1, 2, lambda: calculator.press_key(3))
 
-button('4', 2, 0)
-button('5', 2, 1)
-button('6', 2, 2)
+button('4', 2, 0, lambda: calculator.press_key(4))
+button('5', 2, 1, lambda: calculator.press_key(5))
+button('6', 2, 2, lambda: calculator.press_key(6))
 
-button('7', 3, 0)
-button('8', 3, 1)
-button('9', 3, 2)
+button('7', 3, 0, lambda: calculator.press_key(7))
+button('8', 3, 1, lambda: calculator.press_key(8))
+button('9', 3, 2, lambda: calculator.press_key(9))
 
-button('0', 4, 1)
+button('0', 4, 1, lambda: calculator.press_key(0))
 # end button
 
 #start operation buttons
-divide = button('/', 1, 3)
-multiply = button('*', 2, 3)
-sub = button('-', 3, 3)
-add = button('+', 4, 3) 
+divide = button('/', 1, 3, lambda: calculator.operation('/'))
+multiply = button('*', 2, 3, lambda: calculator.operation('*'))
+sub = button('-', 3, 3, lambda: calculator.operation('-'))
+add = button('+', 4, 3, lambda: calculator.operation('+')) 
 
-equal = button('=', 4, 2)
-cancel = button('C', 4, 0)
+equal = button('=', 4, 2, command = calculator.compute )
+cancel = button('C', 4, 0, lambda: calculator.clear())
 
 
 
